bec14de3956df3448bf6bbf45e00d6ef533ea2a0
Yacine Petitprez <yacine@kosmogo.com>
2013-06-02 21:56:55 +0200

    add therubyracer for deploiement

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 require "rvm/capistrano"
 
 set :application, "happy_photos"
-set :repository,  "git@github.com:anykeyh/happy-photos.git"
+set :repository,  "git://github.com/anykeyh/happy-photos.git"
 set :scm, "git"
 
 set :rvm_type, :system
-set :rvm_ruby_string, "1.9.3"
+set :rvm_ruby_string, "2.0.0"
 set :rvm_path_source, "/usr/local/rvm/bin/rvm"
 set :rvm_path, "/usr/local/rvm"
 ssh_options[:forward_agent] = true
@@ -21,13 +21,13 @@ role :db,  "anykeyh", :primary => true # This is where Rails migrations will run
 # after "deploy:restart", "deploy:cleanup"
 set :deploy_to, "/home/www/#{application}"
 set :use_sudo, false
-set :user, "nginx"
+set :user, "root"
 
 
 task :configure, :roles => :web do
-  run "rm #{current_release}/config/database.yml && ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml"
-  run "rm #{current_release}/config/happy_photos.yml && ln -s #{shared_path}/config/database.yml #{current_release}/config/happy_photos.yml"
-  run "rm -rf #{current_release}/.bundle  && ln -s #{shared_path}/dot_bundle #{current_release}/.bundle"
+  run "(rm #{current_release}/config/database.yml; true) && ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml"
+  run "(rm #{current_release}/config/happy_photos.yml; true) && ln -s #{shared_path}/config/database.yml #{current_release}/config/happy_photos.yml"
+  #run "rm -rf #{current_release}/.bundle  && ln -s #{shared_path}/dot_bundle #{current_release}/.bundle"
 end
 
 
@@ -50,12 +50,12 @@ namespace :deploy do
 
   desc "Stop the Thin processes"
   task :stop do
-    run "source #{rvm_path_source} && cd #{release_path} && kill -QUIT `cat tmp/pids/unicorn.pid`",
+    run "source #{rvm_path_source} && cd #{release_path} && kill -QUIT `cat tmp/pids/unicorn.pid`"
   end
 
   desc "Restart the Thin processes"
   task :restart do
-    run "source #{rvm_path_source} && cd #{release_path} && kill -QUIT `cat tmp/pids/unicorn.pid`",
+    run "source #{rvm_path_source} && cd #{release_path} && kill -QUIT `cat tmp/pids/unicorn.pid`"
     run "source #{rvm_path_source} && cd #{release_path} && unicorn_rails -c config/unicorn.rb -D -E production"
   end
 
@@ -86,11 +86,10 @@ end
 
 def rake(task)
   run [
-    "source #{rvm_path_source} &&", 
+    "source #{rvm_path_source} &&",
     "cd #{release_path} &&",
     "RAILS_ENV=production rake #{task}",
   ].join(" ")
-
 end
 
 
